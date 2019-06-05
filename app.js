7d70255198664e2b64d048f19ef67ebd9c47a71c
Rosa Isela Lopez Palma <rosaiselalopezpalma@MacBook-Air-de-Rosa.local>
2019-06-05 12:09:18 -0500

    Reemplace onclick, agregué variables para preguntas, posibles respuestas y respuestas correctas de GOT, seleccione los h1 y botones vacios del HTML y se los asigé como valor a variables, también hice un for para llenar los botones vacíos con las posibles respuestas y por último hice un for para iterar los botones con la finalidad de encontrar si el botón elegido coincidia con el que contenía la respuesta correcta

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ let userName = localStorage.getItem("userName");
 
 // Guardando el nombre del usuario
 document.getElementById("saveName").addEventListener("click",function saveName() {
-  /*Captura de datos escrito en los inputs*/   
- let nam = document.getElementById("name").value;
- /*Guardando los datos en el LocalStorage*/
- localStorage.setItem("userName",nam);
+    /*Captura de datos escrito en los inputs*/   
+    let nam = document.getElementById("name").value;
+    /*Guardando los datos en el LocalStorage*/
+    localStorage.setItem("userName",nam);
 })
+
+
+
+
+
+
 
 
